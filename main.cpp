b561f37ca8141258124a72688f01e90e84b1b2ae
NemanjaT1992 <nemanjatomasevic71@gmail.com>
2016-06-22 21:11:21 +0200

    fist commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <iostream>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    qDebug() << "asd";
+    return a.exec();
+}
